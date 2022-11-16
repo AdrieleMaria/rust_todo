54fd17e0093567f61b6b41b79024ea37635279f2
Adriele Maria <adrieleifrn@gmail.com>
2022-11-16 11:18:40 -0300

    primeira versão

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
 fn main() {
-    println!("Hello, world!");
+    
+    loop{      
+
+        println!("\nQuer adicionar um novo TODO 📝?");
+
+        println!("digite (sim) para confirmar 👍  ou (nao) para negar 👎");
+
+        let resposta= input();
+
+        if resposta  == "sim" {
+            println!("\nQual TODO 📝 deseja criar?");
+
+            let todo : String = input();    
+
+            println!("\nvocê criou o TODO\n\n 🔹 {}", todo);
+        } else {
+            println!("\nAté a próxima 👋 e volte sempre!🫶");
+            break;
+        }  
+    }
 }
+
+
+
+fn input() -> String {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
+ }
