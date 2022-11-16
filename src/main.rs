0794f9608655865d53f206703b4f006878f672a7
Adriele Maria <adrieleifrn@gmail.com>
2022-11-16 14:22:17 -0300

    pequenas alterações
    formatando com cargo fmt e retirando o tipo string do let todo

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,26 @@
-
 fn main() {
-    
-    loop{      
-
+    loop {
         println!("\nQuer adicionar um novo TODO 📝?");
 
         println!("digite (sim) para confirmar 👍  ou (nao) para negar 👎");
 
-        let resposta= input();
+        let resposta = input();
 
-        if resposta  == "sim" {
+        if resposta == "sim" {
             println!("\nQual TODO 📝 deseja criar?");
 
-            let todo : String = input();    
+            let todo = input();
 
             println!("\nvocê criou o TODO\n\n 🔹 {}", todo);
         } else {
             println!("\nAté a próxima 👋 e volte sempre!🫶");
             break;
-        }  
+        }
     }
 }
-
-
 
 fn input() -> String {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
     buf.trim().to_string()
- }
+}
